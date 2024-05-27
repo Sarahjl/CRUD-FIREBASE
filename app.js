129198976b5e8ce2670ce53704a2fedd0c4469cf
Sarahjl <sjandoza18@gmail.com>
2024-05-27 20:54:55 -0300

    project_files

--- app.js
+++ app.js
@@ -34,16 +34,49 @@ app.get("/consulta", async function(req, res){
 
         res.render("consulta", {agendamentos: agendamentos})
     }catch(erro){
-        console.log("Erro ao consultar dados: " + erro);
+        console.log("Erro ao consultar documento: " + erro);
         res.redirect('/')
     }
 })
 
-app.get("/editar/:id", function(req, res){
+app.get("/editar/:id", async function(req, res){
+    
+    try {
+        const docRef = db.collection('agendamentos').doc(req.params.id);
+        const doc = await docRef.get();
+        if (!doc.exists) {
+          console.log('No such document!');
+          res.status(404).send("Documento não encontrado");
+        } else {
+          res.render("editar", { id: req.params.id, agendamento: doc.data() });
+        }
+      } catch (error) {
+        console.error("Error getting document: ", error);
+        res.status(500).send("Erro ao buscar documento");
+      }
+
 })
 
-app.get("/excluir/:id", function(req, res){
-})
+// app.route("/excluir/:id").get((req, res) => {
+//     db.collection('agendamentos').doc(req.params.id).delete().then(() => {
+//         console.log('Documento excluído com sucesso.');
+//         res.redirect('/consulta');
+//     }).catch((error) => {
+//       console.error('Erro ao excluir o documento: ', error);
+//       res.status(500).send('Erro ao excluir o documento.');
+//     });
+// });
+
+app.get("/excluir/:id", async function (req, res) {
+    try {
+      await db.collection('agendamentos').doc(req.params.id).delete();
+      console.log('Document successfully deleted');
+      res.redirect('/consulta');
+    } catch (error) {
+      console.error("Error deleting document: ", error);
+      res.status(500).send("Erro ao excluir documento");
+    }
+  });
 
 app.post("/cadastrar", function(req, res){
     var result = db.collection('agendamentos').add({
@@ -53,12 +86,28 @@ app.post("/cadastrar", function(req, res){
         data_contato: req.body.data_contato,
         observacao: req.body.observacao
     }).then(function(){
-        console.log('Added document');
+        console.log('Documento inserido com sucesso');
         res.redirect('/')
     })
 })
 
-app.post("/atualizar", function(req, res){
+app.post("/atualizar", async function(req, res){
+    
+    try {
+        const docRef = db.collection('agendamentos').doc(req.body.id).update({
+          nome: req.body.nome,
+          telefone: req.body.telefone,
+          origem: req.body.origem,
+          data_contato: req.body.data_contato,
+          observacao: req.body.observacao
+        });
+        console.log('Document successfully updated');
+        res.redirect('/consulta');
+      } catch (error) {
+        console.error("Error updating document: ", error);
+        res.status(500).send("Erro ao atualizar documento" + error);
+      }
+
 })
 
 app.listen(8081, function(){
